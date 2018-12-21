c590b8b96620bef858f3e5d2a6b740f33483ac8f
Jeff Hegedus <jeff@22ndtech.com>
2018-12-20 21:57:41 -0700

    Updated to work with lumin sdk v0.19.0

--- fragment-shader.glsl
+++ fragment-shader.glsl
@@ -4,6 +4,6 @@ uniform mat4 RotationMatrix;
 
 void main(){
   
-//  color = vec3(1,0,0);
-  color = vec3(RotationMatrix[0][0], RotationMatrix[0][1], RotationMatrix[0][2]);
+  color = vec3(1,0,0);
+//  color = vec3(RotationMatrix[0][0], RotationMatrix[0][1], RotationMatrix[0][2]);
 }
